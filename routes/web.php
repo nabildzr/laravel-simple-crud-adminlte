f95a95e121be6e27bf16307e3d55cc4ef54c8a5d
nabildzr <muhammadnbillz@gmail.com>
2025-04-26 19:22:16 +0700

    refactor login and logout routes to use middleware groups

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,21 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
-Route::get('/login', function () {
-  return view('login');
-})->name('login')->middleware('guest');
-
-Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 
+Route::middleware(['guest'])->group(function () {
+  Route::get('/login', function () {
+    return view('login');
+  })->name('login')->middleware('guest');
+
+  Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
+});
 
 
-Route::middleware(['auth'])->group(function() {
+
+
+
+Route::middleware(['auth'])->group(function () {
   Route::get('/', [KelasController::class, 'index']);
   Route::get('kelas/add', [KelasController::class, 'create']);
   Route::post('kelas/add', [KelasController::class, 'store']);
@@ -33,4 +37,8 @@ Route::middleware(['auth'])->group(function() {
   Route::get('siswa/{id}/edit', [SiswaController::class, 'edit']);
   Route::patch('siswa/{id}/edit', [SiswaController::class, 'update']);
   Route::delete('siswa/{id}/delete', [SiswaController::class, 'destroy']);
+
+  // logout
+
+  Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 });
