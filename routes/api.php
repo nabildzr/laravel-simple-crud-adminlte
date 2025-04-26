c794406d5f1e15e29d009b7e998d8a849056f26d
nabildzr <muhammadnbillz@gmail.com>
2025-04-26 17:54:37 +0700

    remove user authentication route from API

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,3 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
-});
